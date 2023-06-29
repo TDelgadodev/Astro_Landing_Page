2d0aa4ad7c278691efb185fa480e3a21ee72c317
Thiago <thiagodelgado.dev@gmail.com>
2023-06-29 12:47:32 -0300

    astro config deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://TDelgadodev.github.io',
+  base: '/Astro_Landing_Page',
 });
